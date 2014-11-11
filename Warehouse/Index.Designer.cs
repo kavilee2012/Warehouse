99bdf0057c5a96bf17c2674ca23a3471aee513bb
kavilee <kavilee@163.com>
2014-11-12 01:33:30 +0800

    2

--- Warehouse/Index.Designer.cs
+++ Warehouse/Index.Designer.cs
@@ -28,11 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.商品管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.人员管理ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.入库ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.出库ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.商品管理ToolStripMenuItem,
+            this.人员管理ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(786, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 商品管理ToolStripMenuItem
+            // 
+            this.商品管理ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.入库ToolStripMenuItem,
+            this.出库ToolStripMenuItem});
+            this.商品管理ToolStripMenuItem.Name = "商品管理ToolStripMenuItem";
+            this.商品管理ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.商品管理ToolStripMenuItem.Text = "商品管理";
+            // 
+            // 人员管理ToolStripMenuItem
+            // 
+            this.人员管理ToolStripMenuItem.Name = "人员管理ToolStripMenuItem";
+            this.人员管理ToolStripMenuItem.Size = new System.Drawing.Size(68, 21);
+            this.人员管理ToolStripMenuItem.Text = "人员管理";
+            // 
+            // 入库ToolStripMenuItem
+            // 
+            this.入库ToolStripMenuItem.Name = "入库ToolStripMenuItem";
+            this.入库ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.入库ToolStripMenuItem.Text = "入库";
+            // 
+            // 出库ToolStripMenuItem
+            // 
+            this.出库ToolStripMenuItem.Name = "出库ToolStripMenuItem";
+            this.出库ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.出库ToolStripMenuItem.Text = "出库";
+            // 
+            // Index
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 461);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Index";
             this.Text = "Index";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 商品管理ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 入库ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 出库ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 人员管理ToolStripMenuItem;
     }
 }
